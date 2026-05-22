fe399d4eabd1dff4a9921d8f1e317628c901e7ad
Omkar Salunke <salunkeomkar834@gmail.com>
2026-05-22 20:33:01 +0530

    test-15

--- scripts/code_review.py
+++ scripts/code_review.py
@@ -4,13 +4,33 @@ from google import genai
 import smtplib
 from email.message import EmailMessage
 
+from torch import diff
+
 
 def getdiff():
     diff = subprocess.check_output(['git','show'], text=True, )
     return diff
 
 
-clinet = genai.Client()
+client = genai.Client()
+
+
+
+
+def get_commit_info():
+
+    commit = subprocess.check_output(
+        ["git", "log", "-1", "--pretty=format:%H%n%an%n%s"],
+        text=True
+    )
+
+    commit_id, author, message = commit.split("\n")
+
+    return {
+        "commit_id": commit_id,
+        "author": author,
+        "message": message
+    }
 
 
 def send_email(html_content):
@@ -31,18 +51,77 @@ def send_email(html_content):
     return "Email sent successfully!"
 
 
-
- 
-
 def main():
-    diff = getdiff()
-    promt = f"Review the following code changes and provide feedback:\n\n mandatory : provide a output in html format that can used to send as an email :\n\n{diff}"
-    response = clinet.models.generate_content(
-        model="gemini-3-flash-preview",
-        contents=promt
-    )
-    html = response.text
-    send_email(html)
-    
 
-main()
+    diff = getdiff()
+
+    commit_info = get_commit_info()
+
+    prompt = f"""
+You are a senior AI code reviewer.
+
+Commit Details:
+Author: {commit_info['author']}
+Commit Message: {commit_info['message']}
+Commit ID: {commit_info['commit_id']}
+
+Analyze the following git code changes.
+
+Check for:
+1. Bugs and logical issues
+2. Security vulnerabilities
+3. Performance optimization
+4. Code readability
+5. Best practices
+
+Provide:
+- Summary
+- Issues found
+- Severity level
+- Suggested fixes
+- Final score out of 10
+
+
+IMPORTANT:
+
+Return ONLY clean and modern HTML email format.
+
+Use:
+- colorful UI
+- modern cards
+- proper headings
+- tables
+- sections
+- severity colors
+- padding and spacing
+
+Color Rules:
+- Critical = Red
+- Medium = Orange
+- Low = Green
+- Headers = Blue
+
+Add:
+1. Project title
+2. Commit details section
+3. AI review summary
+4. Issues found section
+5. Suggested fixes section
+6. Final score card
+
+Use inline CSS styling.
+
+Make the email visually beautiful and professional.
+
+Git Diff:
+{diff}
+"""
+
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents=prompt
+    )
+
+    html = response.text
+
+    send_email(html)
